490b4ad6b55b63d4860740583fa800f09b71523e
carlos fernandes <carlosfernandes6@outlook.com>
2023-12-26 16:12:28 -0300

    style: estilizar o header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,10 @@
+import styles from './Header.module.css'
+
 function Header() {
     const dateTime = new Date
     return(
-        <header>
-            <h1>{`${dateTime.getDay()}-${dateTime.getMonth()}-${dateTime.getFullYear()}`}</h1>
+        <header className={styles['header-header']}>
+            <h1 className={styles['header-h1']}>{`${dateTime.getDay()}-${dateTime.getMonth()}-${dateTime.getFullYear()}`}</h1>
         </header>
     )
 }
