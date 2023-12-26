adec01acadc68fb4d77c017dbb598dbd912d0001
carlos fernandes <carlosfernandes6@outlook.com>
2023-12-26 16:50:03 -0300

    style: estilizar a task

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,13 +1,35 @@
+import { useState } from 'react'
+import styles from './Task.module.css'
+
 interface TaskProps {
     id?: number,
     content: string
 }
 
 function Task(props:TaskProps) {
+    const [checkboxStatus, setCheckboxStatus] = useState(false)
+
     return(
-        <li>
-            <input type="checkbox" name={props.id.toString()} id={props.id.toString()} />
-            <p>{props.content}</p>
+        <li className={styles['task-li']}>
+            <input 
+                className={styles['task-checkbox']}
+                type="checkbox" 
+                name={props.id.toString()} 
+                id={props.id.toString()} 
+                checked={checkboxStatus}
+                onChange={()=>{
+                    if(checkboxStatus == false) setCheckboxStatus(true)
+                    if(checkboxStatus == true) setCheckboxStatus(false)
+                }}
+            />
+            <p className={styles['task-p']}
+                style={{
+                    textDecoration: (checkboxStatus)? 'line-through': 'none',
+                    opacity: (checkboxStatus)? '70%': '100%'
+                }}
+            >
+                {props.content}
+            </p>
         </li>
     )
 }
