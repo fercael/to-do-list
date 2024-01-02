8d989dac8addae2607af1749a5f0038e10233e57
carlos fernandes <carlosfernandes6@outlook.com>
2024-01-02 07:53:02 -0300

    fix: corrigir exibicao da data no header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ function Header() {
     const dateTime = new Date
     return(
         <header className={styles['header-header']}>
-            <h1 className={styles['header-h1']}>{`${dateTime.getDay()}-${dateTime.getMonth()}-${dateTime.getFullYear()}`}</h1>
+            <h1 className={styles['header-h1']}>{`${dateTime.getDay()}-${dateTime.getMonth() + 1}-${dateTime.getFullYear()}`}</h1>
         </header>
     )
 }
