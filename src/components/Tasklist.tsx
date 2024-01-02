7488666d0433d52bfcc400b4f5e31549ccb0daed
carlos fernandes <carlosfernandes6@outlook.com>
2024-01-02 07:48:11 -0300

    style: estilizar a tasklist

--- src/components/Tasklist.tsx
+++ src/components/Tasklist.tsx
@@ -1,29 +1,31 @@
 import { useState } from "react"
 import { Task } from "./Task";
 import { PlusCircle } from "@phosphor-icons/react"
+import styles from "./Tasklist.module.css"
 
 
 function Tasklist() {
     const [listOfTasks, setListOfTasks] = useState(Array<string>)
     const [newTask, setNewTask] = useState('')
     return(
-        <section>
-            <ul>
+        <section className={styles['tasklist-section']}>
+            <ul className={styles['tasklist-ul']}>
                 {listOfTasks.map((content, index) => {
                     return(
                         <Task id={index} content={content}/>
                     )
                 })}
             </ul>
-            <input type="text" name="list-input" id="list-input" value={newTask} onChange={e=>{
+            <input className={styles['tasklist-input']} type="text" name="list-input" id="list-input" value={newTask} onChange={e=>{
                 setNewTask(e.target.value)
             }}/>
             <button
+                className={styles['tasklist-button']}
                 onClick={()=>{
                    setListOfTasks([...listOfTasks, newTask])
                 }}
             >
-                <PlusCircle />
+                <PlusCircle className={styles['tasklist-icon']} />
             </button>
         </section>
     )
